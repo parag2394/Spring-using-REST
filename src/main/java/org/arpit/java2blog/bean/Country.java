2a775bfa051baa6d983dec741ffebdee037234b8
Parag <Parag@LAPTOP-PARAG>
2018-04-17 15:50:03 +0530

    Spring REST CRUD Example

---
+++ src/main/java/org/arpit/java2blog/bean/Country.java
@@ -0,0 +1,39 @@
+package org.arpit.java2blog.bean;
+
+public class Country{
+	
+	int id;
+	String countryName;	
+	long population;
+	
+	public Country(){
+		super();		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public long getPopulation() {
+		return population;
+	}
+	public void setPopulation(long population) {
+		this.population = population;
+	}
+	public Country(int id, String countryName, long population) {
+		super();
+		this.id = id;
+		this.countryName = countryName;
+		this.population = population;
+	}
+	
+	
+	
+}
